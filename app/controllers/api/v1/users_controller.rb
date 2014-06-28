f5fd3ed879761bec0497fb77b457b35b9a6a2cb6
Ravi Asnani <asnafam@gmail.com>
2014-06-29 02:22:20 +0530

    API: Registration / Login / Update - User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,21 +3,23 @@ class Api::V1::UsersController < ApiBaseController
 
   def create
     @user = User.find_by_email(user_params[:email]) 
-    if @user.present?
-      if @user.update_attributes(user_params)
-        render 'created.json.jbuilder'
-      else
-        render json: { error: { code: ERROR_UNPROCESSABLE, messages: @user.errors.full_messages } }, status: :unprocessable_entity
-      end
-    else
+    unless @user.present?
       @user = User.new(user_params)
       @user.password = "yobitch"
-      if @user.save
-        sign_in @user
-        render 'created.json.jbuilder'
-      else
-        render json: { error: { code: ERROR_UNPROCESSABLE, messages: @user.errors.full_messages } }, status: :unprocessable_entity
-      end
+    end
+    if @user.save
+      sign_in @user
+      render 'created.json.jbuilder'
+    else
+      render json: { error: { code: ERROR_UNPROCESSABLE, messages: @user.errors.full_messages } }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      render 'created.json.jbuilder'
+    else
+      render json: { error: { code: ERROR_UNPROCESSABLE, messages: @user.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
--- app/views/api/v1/users/created.json.jbuilder
+++ app/views/api/v1/users/created.json.jbuilder
@@ -1,1 +1,9 @@
-json.user @user
+json.user do
+  json.id @user.id
+  json.email @user.email
+  json.name @user.name
+  json.is_free @user.is_free
+  json.is_elevated @user.is_elevated
+  json.gcm_token @user.gcm_token
+  json.auth_token @user.auth_token
+end
