25d60d98f6fa29b085e87bd2d7ee61d3116d2923
Ravi Asnani <asnafam@gmail.com>
2014-08-03 11:40:56 +0530

    Mailers

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -2,6 +2,7 @@ class SystemMailer < ActionMailer::Base
   default from: "Yo! B*tch <hello@yobitch.me>"
 
   def welcome(user)
+    @user = user
     mail(
       to: "#{user.name} <#{user.email}>", 
       subject: 'Welcome to Yo! B*tch'
